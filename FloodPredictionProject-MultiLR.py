2aa8151e376869948e1c26ed42b02886a2c95f6c
NaziyaShaik30 <161419324+NaziyaShaik30@users.noreply.github.com>
2026-09-02 20:03:20 +0530

    Refactor Flood Prediction script for clarity and structure

--- FloodPredictionProject-MultiLR.py
+++ FloodPredictionProject-MultiLR.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-
+#reading the csv files 
 data=pd.read_csv("Project_test.csv")
 data1=pd.read_csv("Project_train.csv")
 #data types of the features
@@ -24,7 +24,7 @@ print(data1.isnull().sum())
 print(data.head())
 print(data1.head())
 #heatmap
-#correlation between the features and target
+#co-relation between the features and target
 plt.figure(dpi=125)
 sns.heatmap(np.round(data1.corr(numeric_only=True),2),annot=True)
 plt.show()
@@ -33,17 +33,17 @@ x=data1[['MonsoonIntensity','TopographyDrainage','RiverManagement','Deforestatio
 y=data1['FloodProbability']
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
-#model selection
+#model selection- Multi Linear Regression
 model=LinearRegression()
 #training or fitting the data
 model.fit(x_train,y_train)
-
 #predicting
 y_pred=model.predict(x_test)
 
 # acc=accuracy_score(y_test,y_pred)
 # print(f"accuracy ",acc)
 
+#regression plot between the features and the target of the data
 plt.figure(dpi=125)
 sns.regplot(x=y_test, y=y_pred, scatter_kws={"color": "blue"}, line_kws={"color": "red"})
 plt.xlabel('True Values')
@@ -51,23 +51,13 @@ plt.ylabel('Predicted Values')
 plt.title('Regression Line for y_test vs y_pred')
 plt.show()
 
-
+#graphs between the features and target to know the dependence 
 plt.scatter(data1['RiverManagement'],data1['FloodProbability'])
 plt.xlabel('RiverManagement')  # Label for x-axis
 plt.ylabel('FloodProbability')  # Label for y-axis
 plt.title('Scatter Plot of  RiverManagement vs FloodProbability  ')
 plt.show()
 
-plt.scatter(data1['Deforestation'],data1['FloodProbability'])
-plt.xlabel('Deforestation')  # Label for x-axis
-plt.ylabel('FloodProbability')  # Label for y-axis
-plt.title('Scatter Plot of  Deforestation vs FloodProbability  ')
-plt.show()
-plt.scatter(data1['Urbanization'],data1['FloodProbability'])
-plt.xlabel('Urbanization')  # Label for x-axis
-plt.ylabel('FloodProbability')  # Label for y-axis
-plt.title('Scatter Plot of  Urbanization vs FloodProbability  ')
-plt.show()
 plt.scatter(data1['ClimateChange'],data1['FloodProbability'])
 plt.xlabel('ClimateChange')  # Label for x-axis
 plt.ylabel('FloodProbability')  # Label for y-axis
@@ -75,7 +65,7 @@ plt.title('Scatter Plot of  ClimateChange vs FloodProbability  ')
 plt.show()
 plt.figure(figsize=(20, 15))
 
-# Iterate over each feature and create a box plot
+#mean squared error to the error of the prediction data
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 print(f"mean squared error",mse)
